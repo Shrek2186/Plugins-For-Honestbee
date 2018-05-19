064fa0d472fb57f07bf30761f890758e93f54e4a
Shrek Huang <32711959+Shrek2186@users.noreply.github.com>
2018-05-20 05:33:09 +0800

    Add files via upload

---
+++ DEAR/popup.js
@@ -0,0 +1,12 @@
+﻿var options = {
+    type: "basic",
+    title: "Honestbee浮動式窗開啟通知",
+    message: "Hello Taiwan",
+    iconUrl: "icon.png"
+};
+
+chrome.notifications.create(options, callback);
+function callback() {
+    
+    console.log('popup done');
+}
